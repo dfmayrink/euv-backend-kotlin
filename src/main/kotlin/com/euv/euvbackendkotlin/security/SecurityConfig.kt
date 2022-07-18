8d7c545c170c09578a59bf336b6a96aa5fda845d
Dereck <dereck.mayrink@gmail.com>
2022-07-18 07:46:43 -0300

    Testing graphql

--- src/main/kotlin/com/euv/euvbackendkotlin/security/SecurityConfig.kt
+++ src/main/kotlin/com/euv/euvbackendkotlin/security/SecurityConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.ReactiveAuthenticationManager
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
@@ -24,6 +25,7 @@ import reactor.core.publisher.Mono
 
 @Configuration
 @EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 class SecurityConfig {
 
     @Autowired
